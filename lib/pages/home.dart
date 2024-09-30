73981db118e0d25b356f54a6e747c30346a75bbf
chakshit2004h <84057123+chakshit2004h@users.noreply.github.com>
2024-09-30 13:55:27 +0530

    Initial commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -8,9 +27,132 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  // Define a GlobalKey for the Scaffold
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey, // Attach the key to Scaffold
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF141527), // Same background color
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.menu, color: Colors.white),
+              onPressed: () {
+                // Use the Scaffold key to open the drawer
+                _scaffoldKey.currentState?.openDrawer();
+              },
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Container(
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 10),
+                      const Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Search...',
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.search),
+                        color: Colors.black, // Search button color
+                        onPressed: () {
+                          // Handle search action
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: const Color(0xFF292B40), // Background color
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Color(0xFF292B40),
+              ),
+              child: const Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontFamily: "ibm",
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home, color: Colors.white),
+              title: const Text('Home', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.group, color: Colors.white),
+              title: const Text('Communities', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.pop(context); // Handle communities tap
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.feed, color: Colors.white),
+              title: const Text('Feed', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.pop(context); // Handle feed tap
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person_pin_circle, color: Colors.white),
+              title: const Text('Travel Buddy', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.pop(context); // Handle travel buddy tap
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.map, color: Colors.white),
+              title: const Text('Travel Guide', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.pop(context); // Handle travel guide tap
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.hotel, color: Colors.white),
+              title: const Text('Hotels', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.pop(context); // Handle hotels tap
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.info, color: Colors.white),
+              title: const Text('About', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.pop(context); // Handle about tap
+              },
+            ),
+          ],
+        ),
+      ),
       body: SingleChildScrollView(
         child: ConstrainedBox(
           constraints: BoxConstraints(
@@ -25,57 +167,6 @@ class _HomeState extends State<Home> {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // Menu Icon
-                      IconButton(
-                        icon: const Icon(Icons.menu, color: Colors.white),
-                        onPressed: () {},
-                      ),
-                      // Search Field with Button
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                          child: Container(
-                            height: 40,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Row(
-                              children: [
-                                const SizedBox(width: 10),
-                                const Expanded(
-                                  child: TextField(
-                                    decoration: InputDecoration(
-                                      hintText: 'Search...',
-                                      border: InputBorder.none,
-                                    ),
-                                  ),
-                                ),
-                                IconButton(
-                                  icon: const Icon(Icons.search),
-                                  color: Colors.black, // Search button color
-                                  onPressed: () {
-                                    // Handle search action
-                                  },
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      // Ring Icon
-                      IconButton(
-                        icon: const Icon(Icons.notifications, color: Colors.white),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                ),
                 const SizedBox(height: 30),
                 const Padding(
                   padding: EdgeInsets.only(left: 20.0),
@@ -101,7 +192,7 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                 ),
-                const SizedBox(height: 30,),
+                const SizedBox(height: 30),
                 Padding(
                   padding: const EdgeInsets.only(left: 70.0),
                   child: Container(
@@ -113,10 +204,9 @@ class _HomeState extends State<Home> {
                     ),
                     child: Column(
                       children: [
-                        // Centering only the image
                         Center(
                           child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20), // Circular borders
+                            borderRadius: BorderRadius.circular(20),
                             child: Padding(
                               padding: const EdgeInsets.only(top: 10.0),
                               child: Image.asset(
@@ -128,7 +218,6 @@ class _HomeState extends State<Home> {
                             ),
                           ),
                         ),
-                        // Text remains aligned as before
                         const Padding(
                           padding: EdgeInsets.only(left: 15.0, top: 8.0),
                           child: Text(
@@ -142,19 +231,30 @@ class _HomeState extends State<Home> {
                           ),
                         ),
                         Container(
-                          width:230,
+                          width: 230,
                           height: 36,
                           decoration: const BoxDecoration(
                             color: Color(0xFFD2F366),
-                            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(20),
+                              bottomRight: Radius.circular(20),
+                            ),
                           ),
-                          child: const Center(child: Text("Starting from 19600",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500),)),
+                          child: const Center(
+                            child: Text(
+                              "Starting from 19600",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
                         ),
                       ],
                     ),
                   ),
                 ),
-                const SizedBox(height: 30,),
+                const SizedBox(height: 30),
                 Padding(
                   padding: const EdgeInsets.only(left: 70.0),
                   child: Container(
@@ -166,10 +266,9 @@ class _HomeState extends State<Home> {
                     ),
                     child: Column(
                       children: [
-                        // Centering only the image
                         Center(
                           child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20), // Circular borders
+                            borderRadius: BorderRadius.circular(20),
                             child: Padding(
                               padding: const EdgeInsets.only(top: 10.0),
                               child: Image.asset(
@@ -181,7 +280,6 @@ class _HomeState extends State<Home> {
                             ),
                           ),
                         ),
-                        // Text remains aligned as before
                         const Padding(
                           padding: EdgeInsets.only(left: 15.0, top: 8.0),
                           child: Text(
@@ -195,19 +293,30 @@ class _HomeState extends State<Home> {
                           ),
                         ),
                         Container(
-                          width:230,
+                          width: 230,
                           height: 36,
                           decoration: const BoxDecoration(
                             color: Color(0xFFD2F366),
-                            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(20),
+                              bottomRight: Radius.circular(20),
+                            ),
                           ),
-                          child: const Center(child: Text("Starting from 19580",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500),)),
+                          child: const Center(
+                            child: Text(
+                              "Starting from 19580",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
                         ),
                       ],
                     ),
                   ),
                 ),
-                const SizedBox(height: 30,),
+                const SizedBox(height: 30),
                 Padding(
                   padding: const EdgeInsets.only(left: 70.0),
                   child: Container(
@@ -219,10 +328,9 @@ class _HomeState extends State<Home> {
                     ),
                     child: Column(
                       children: [
-                        // Centering only the image
                         Center(
                           child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20), // Circular borders
+                            borderRadius: BorderRadius.circular(20),
                             child: Padding(
                               padding: const EdgeInsets.only(top: 10.0),
                               child: Image.asset(
@@ -234,7 +342,6 @@ class _HomeState extends State<Home> {
                             ),
                           ),
                         ),
-                        // Text remains aligned as before
                         const Padding(
                           padding: EdgeInsets.only(left: 15.0, top: 8.0),
                           child: Text(
@@ -248,18 +355,30 @@ class _HomeState extends State<Home> {
                           ),
                         ),
                         Container(
-                          width:230,
+                          width: 230,
                           height: 36,
                           decoration: const BoxDecoration(
                             color: Color(0xFFD2F366),
-                            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(20),
+                              bottomRight: Radius.circular(20),
+                            ),
                           ),
-                          child: const Center(child: Text("Starting from 20600",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500),)),
+                          child: const Center(
+                            child: Text(
+                              "Starting from 15000",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
                         ),
                       ],
                     ),
                   ),
                 ),
+                const SizedBox(height: 30),
               ],
             ),
           ),
